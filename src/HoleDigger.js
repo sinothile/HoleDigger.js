4931ed49b2b0c16a3793b32868fdd4e652b033d5
Sinothile Mbatha <SinothileM@SAHOMELOANS.COM>
2018-06-12 08:16:28 +0200

    wip

--- src/HoleDigger.js
+++ src/HoleDigger.js
@@ -8,23 +8,16 @@ function ChoosePlan() {
         let reusedHoles = [];
         let reusedLength;
 
-        for (let i = 0; i <= length; i += oldDistance) {
-            if (newDistance[i] >= oldDistance) {
-                return null;
-            }
-            existingHolesArray.push(i);
+        if (newDistance >= oldDistance) {
+            return null;
         }
 
-        for (let j = 0; j <= length; j += newDistance[0]) {
-            newHolesArray.push(j);
-        }
-        existingHolesArray.forEach((e1) => newHolesArray.forEach((e2) => {
-            if (e1 === e2) {
-                reusedHoles.push(e1)
-            }
-        }));
+        PopulateOldDistance(length,oldDistance,newDistance,existingHolesArray);
+        PopulateNewDistance (length,newDistance,newHolesArray);
+        ReusedHoles(existingHolesArray,newHolesArray,reusedHoles);
+        
 
-        let buried = existingHolesArray.filter(h => newHolesArray.indexOf(h) == -1);
+        let buried = existingHolesArray.filter(h => newHolesArray.indexOf(h) == -1);  
         let buriedLength= buried.length;
 
         reusedLength = reusedHoles.length;
@@ -33,6 +26,32 @@ function ChoosePlan() {
         newHoles =Math.floor(holesToBeDug - reusedLength);
 
         return "He needs to dig " + newHoles + " holes and bury " + buriedLength + " old holes";
+    }
+
+    let PopulateOldDistance = function (length,oldDistance,newDistance,existingHolesArray)
+    {
+        for (let i = 0; i <= length; i += oldDistance) { 
+            existingHolesArray.push(i);
+        }
+        return existingHolesArray;
+    }
+
+    let PopulateNewDistance = function (length,newDistance,newHolesArray)
+    {
+        for (let j = 0; j <= length; j += newDistance[0]) {
+            newHolesArray.push(j);
+        }
+        return newHolesArray;
+    }
+
+    let ReusedHoles = function (existingHolesArray,newHolesArray,reusedHoles)
+    {
+        existingHolesArray.forEach((e1) => newHolesArray.forEach((e2) => {
+            if (e1 === e2) {
+                reusedHoles.push(e1)
+            }
+        }));
+        return reusedHoles;
     }
 
 }
